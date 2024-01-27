ded3fc5b2348d17c513b2d40bcf5faafff960e75
nullonspeed <sebastian.langschneider@gmail.com>
2024-01-27 15:53:50 +0100

    Add project files.

---
+++ MobileRobotikUCLibrary/UserControl1.xaml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MobileRobotikUCLibrary
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_forwar(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_stop(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_backward(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
